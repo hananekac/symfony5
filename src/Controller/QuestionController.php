ebd339ec6e3db274ae263ea0eb6c27127dfdac40
hkacemi <hanane.kacemi@gmail.com>
2021-12-09 09:47:25 +0100

    create new question, make show and HP dynamic

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,32 +26,60 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(QuestionRepository $repository)
     {
-        return $this->render('question/homepage.html.twig');
+        $questions = $repository->findBy([], ['askedAt' => 'DESC']);
+        return $this->render('question/homepage.html.twig', ['questions' => $questions]);
     }
 
     /**
+     * @Route ("/questions/new", name="app_question_create")
+     */
+    public function new(EntityManagerInterface $entityManager){
+        $question = new Question();
+        $random = rand(1,100);
+        $question->setName("question n°".$random);
+        $question->setSlug("question-n-".$random);
+        $question->setQuestion(<<< EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+        );
+
+        if ($random > 30) {
+            $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush($question);
+        return new Response(sprintf('id object : %d  question : %s',$question->getId(), $question->getName()));
+    }
+    /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
+        $repository = $entityManager->getRepository(Question::class);
+        $question = $repository->findOneBy(['slug'=> $slug]);
 
+        if(!$question)
+            throw $this->createNotFoundException('404 Not found !');
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-
-        $parsedQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $parsedQuestionText,
+            'question' => $question,
             'answers' => $answers,
         ]);
     }
