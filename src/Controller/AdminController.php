091246c2cff6518d127856d98d7b0430b2e892f8
hkacemi <hanane.kacemi@gmail.com>
2022-01-30 20:22:48 +0100

    auth sys:access control and entry Points

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function dashboard()
+    {
+        return new Response("access to dashboad");
+    }
+
+
+}
